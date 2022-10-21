030884b32c898c6d4883d9ad1e174b7c30c4be4a
kannan <kannanbsk1609080@gmail.com>
2022-10-21 09:39:34 +0530

    Add project files.

---
+++ calculatorlibjenkinsdemo/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatorlibjenkinsdemo
+{
+    public class Class1
+    {
+        void cal()
+        {
+            int a=10, b=20;
+            Console.WriteLine("addition",a+b);
+            Console.WriteLine("subtraction",a-b);
+            Console.WriteLine("multiplivation",a*b);
+            Console.WriteLine("division",a/b);
+        }
+    }
+}
